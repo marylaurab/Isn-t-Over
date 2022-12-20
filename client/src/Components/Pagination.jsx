a2699d45ad6ced513c234e0608ac82e6605d62a8
marylaurab <mbetancourt@fi.uba.ar>
2022-12-20 19:42:22 -0400

    in progress pagination

--- client/src/Components/Pagination.jsx
+++ client/src/Components/Pagination.jsx
@@ -6,61 +6,75 @@ export default function Pagination({
   setCurrentPage,
   currentPage,
 }) {
-  const [limitsToRender, setLimitsToRender] = useState({
-    minLimit: 0,
-    maxLimit: 3,
-  });
-  const [perLimit, setPerLimit] = useState(3);
-  const [currentLimit, setCurrentLimit] = useState(1);
+  const [perSubPages, setPerSubPages] = useState(3);
   const totalPagesToRender = Math.ceil(totalGames / perPage);
-  const conditional = Math.ceil(totalPagesToRender / perLimit);
+  const conditional = Math.ceil(totalPagesToRender / perSubPages);
   const pages = [];
-
+  
   for (let i = 1; i <= totalPagesToRender; i++) {
     pages.push(i);
   }
+  
+  const [currentSubPage, setCurrentSubPage] = useState(1);
+  let j = 0;
+  let k = 1;
+  let l = 2;
+  const minLimit = 0;
+  const maxLimit = 3;
+  const [renderSubPages, setRenderSubPages] = useState([
+    pages[j],
+    pages[k],
+    pages[l],
+  ]);
+
   const previosPage = () => {
     if (currentPage === 1) return;
     setCurrentPage(currentPage - 1);
   };
   const nextPage = () => {
-    if (currentPage === totalPagesToRender) return;
+    if (currentPage > totalPagesToRender) return;
     setCurrentPage(currentPage + 1);
   };
   const onSpecificPage = (page) => {
     setCurrentPage(page);
   };
-  const previousLimit = () => {
-    if (currentLimit === 1) return;
-    setLimitsToRender(
-      (limitsToRender.minLimit = limitsToRender.minLimit - 3,
-        limitsToRender.maxLimit = limitsToRender.maxLimit - 3)
-    );
-    setCurrentLimit(currentLimit-1)
+  const previousSubPages = () => {
+    if (currentSubPage === 1) return;
+    setRenderSubPages([
+      pages[j - perSubPages],
+      pages[k - perSubPages],
+      pages[l - perSubPages],
+    ]);
+    setCurrentSubPage(currentSubPage - 1);
   };
-  const nextLimit = () => {
-    if (conditional === currentLimit) return;
-    setLimitsToRender(
-      (limitsToRender.minLimit = limitsToRender.minLimit + 3,
-      limitsToRender.maxLimit = limitsToRender.maxLimit + 3)
-    );
-    setCurrentLimit(currentLimit+1)
+  const nextSubPages = () => {
+    if (conditional === currentSubPage) return;
+    if(pages[j + perSubPages]===pages[pages.length-1]) {
+      setRenderSubPages([
+        pages[j + perSubPages]
+      ]); 
+      return; 
+    }
+    setRenderSubPages([
+      pages[j + perSubPages],
+      pages[k + perSubPages],
+      pages[l + perSubPages],
+    ]);
+    setCurrentSubPage(currentSubPage + 1);
   };
   return (
     <div>
-      <button onClick={previousLimit}>{"<<"}</button>
+      <button onClick={previousSubPages}>{"<<"}</button>
       <button onClick={previosPage}>Prev</button>
       <ul>
-        {pages
-          .slice(limitsToRender.minLimit, limitsToRender.maxLimit)
-          .map((p) => (
-            <li key={p}>
-              <button onClick={() => onSpecificPage(p)}>{p}</button>
-            </li>
-          ))}
+        {renderSubPages.slice(minLimit, maxLimit).map((p) => (
+          <li key={p}>
+            <button onClick={() => onSpecificPage(p)} key={p}>{p}</button>
+          </li>
+        ))}
       </ul>
       <button onClick={nextPage}>Next</button>
-      <button onClick={nextLimit}>{">>"}</button>
+      <button onClick={nextSubPages}>{">>"}</button>
     </div>
   );
 }
