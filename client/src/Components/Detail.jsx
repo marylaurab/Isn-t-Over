dc47e560c4adca4c9a6c61090c73a0855be7e2ff
marylaurab <mbetancourt@fi.uba.ar>
2023-01-10 20:00:34 -0400

    detail card in progress

--- client/src/Components/Detail.jsx
+++ client/src/Components/Detail.jsx
@@ -1,6 +1,7 @@
 import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import { resetDetailGame } from "../Redux/actions/resets";
+import style from '../cssComponents/detail.module.css'
 
 export default function Detail() {
   const detailGame = useSelector((state) => state.mainData.detailGame);
@@ -11,36 +12,63 @@ export default function Detail() {
   };
 
   return detailGame && Object.keys(detailGame).length > 0 ? (
-    <div>
-      <Link to="/videogames">
-        <button onClick={toBack}>back home</button>
-      </Link>
-      <div>
-        <h1>{`${detailGame.title}`}</h1>
+    <div className={style.container}>
+      <div className={style.backBar}>
+        <Link to="/videogames">
+          <button onClick={toBack} className={style.backButton}>back home</button>
+        </Link>
       </div>
-      <img
-        src={`${detailGame.image}`}
-        width="600px"
-        height="600px"
-        alt={`photo id game: ${detailGame.id}`}
-      />
-      <div>
-        <h6>RELEASE: {`${detailGame.release}`}</h6>
-        <p>{`${detailGame.rating}`}</p>
+      <div className={style.detailCard}>
+        <div>
+          <h1>{`${detailGame.title}`}</h1>
+        </div>
+        <div>
+          <img
+            src={`${detailGame.image}`}
+            width="150px"
+            height="150px"
+            alt={`photo id game: ${detailGame.id}`}
+          />
+        </div>
+        <div>
+          <div>
+            {" "}
+            <h6>RELEASE:</h6>
+          </div>
+          <div>{`${detailGame.release}`}</div>
+        </div>
+        <div>
+          <div>
+            <h6>RATING:</h6>
+          </div>
+          <div>
+            <p>{`${detailGame.rating}`}</p>
+          </div>
+        </div>
+        <div>
+          <div>
+            <h6>PLATFORMS:</h6>
+          </div>
+          {detailGame.platforms?.map((p, i) => (
+            <div key={i}>
+              <span key={i}>{p} </span>
+            </div>
+          ))}
+        </div>
+        <div>
+          <div>
+            <h6>GENRES:</h6>
+          </div>
+          {detailGame.genres?.map((g, i) => (
+            <div key={i}>
+              <span key={i}>{g.name ? g.name : g} </span>
+            </div>
+          ))}
+        </div>
+        <div>
+          <p>{`${detailGame.description}`}</p>
+        </div>
       </div>
-      <div>
-        <h6>PLATFORMS:</h6>
-        {detailGame.platforms?.map((p, i) => (
-          <span key={i}>{p} </span>
-        ))}
-      </div>
-      <div>
-        <h6>GENRES:</h6>
-        {detailGame.genres?.map((g, i) => (
-          <span key={i}>{g.name ? g.name : g} </span>
-        ))}
-      </div>
-      <p>{`${detailGame.description}`}</p>
     </div>
   ) : (
     <img src="https://www.globalreporting.org/styles/assets/images/circle-loading-gif.gif" />
